cd7bafcbac7b798f264096bebcb7d7e37e9aaa29
Simon <simon.duperray@reseau.eseo.fr>
2022-02-14 09:05:47 +0100

    :sparkles: redirection to dashboard after form submition without testing the get request from api

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static("public"))
+// app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.set("view engine", "ejs")
 
-const userRouter = require("./routes/users")
+app.get('/', (req, res, next) => {
+    console.debug("> Homepage");
+    res.render("homepage/home");
+});
 
-app.use("/users", userRouter)
+app.post("/", (req, res, next) => {
+    console.debug("> form submitted");
+    console.log(req.statusCode);
+    console.log(req.body);
+    res.redirect(`/${ req.body.devId }`);
+});
+
+app.get("/:devId", (req, res, next) => {
+    res.render("users/dashboard", { developer: req.params.devId });
+});
+
+// app.post('/users', (req, res, next) => {
+//     console.debug("> debug");
+//     console.log(req.params);
+//     res.redirect(`/users/${ req.params.devId }`);
+// });
 
 app.listen(3000)
