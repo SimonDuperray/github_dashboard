7c8d99e837dcfd5ec6b1709e882ffee7d438b641
Simon <simon.duperray@reseau.eseo.fr>
2022-02-16 10:50:51 +0100

    :lipstick: arrange css of alert

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ require('dotenv').config()
 const express = require("express")
 const axios = require("axios")
 const app = express()
-// var session = require('express-session')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// app.use(session({
-//     isUserExists: true
-// }));
 
 app.set("view engine", "ejs")
 
