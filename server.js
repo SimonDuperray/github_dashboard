529dc3e6c85d7774b7ef16fb792d2138be849fd7
Simon <simon.duperray@reseau.eseo.fr>
2022-02-18 13:43:23 +0100

    :sparkles: add warning alert when limit reached

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const headers = {
     'Accept': 'application/vnd.github.v3.raw',
     'Authorization': 'token '+TOKEN
 }
+var ID;
+var MAX_PAGES=1;
 var DEVELOPER = "";
 var languages_values;
 const colorsGraph = [
@@ -115,12 +117,16 @@ app.post("/", (req, res, next) => {
     axios.get(`https://api.github.com/users/${ req.body.devId }`, headers).then(response => {
         console.log(response.status)
         if(response.status==200){
+            ID = response.data.id
+            console.log(`> dev ${req.body.devId} has the following github id: ${ ID }`)
             DEVELOPER = req.body.devId
             res.redirect(`/dev/${ req.body.devId }`)
         }
     }).catch(error => {
         console.error("> Error detected during the request")
-        res.render('homepage/home', { isUserExists: false, developer: req.body.devId })
+        let limitReached = true ? Object.keys(res).includes('maxRequestsOnConnectionReached') : false
+        console.log(Object.keys(res))
+        res.render('homepage/home', { isUserExists: false, limitReached: limitReached, developer: req.body.devId })
     })
 });
 
@@ -143,111 +149,125 @@ app.get("/dev/:devId", (param_req, param_res, next) => {
     console.log(`The following request will be sent: https://api.github.com/users/${ param_req.params.devId }/repos`)
     console.log(`DEVELOPER: ${ DEVELOPER }`)
   
-    axios.get(`https://api.github.com/users/${ DEVELOPER }/repos`).then(response => {
-        console.log(`> response status for user repos: ${ response.status }`)
-        RESPONSE = response.data
-        console.log(`> response length: ${ RESPONSE.length }`)
-
-
-
-        // REPOS SIZES
-        sizes = new Array()
-        for (var i = 0; i < RESPONSE.length; i++) {
-            sizes.push(RESPONSE[i].size)
-        }
-        sum = sizes.reduce((a, b) => a+b, 0)
-        avg_size = Number(((sum / sizes.length)/1000).toFixed(1)) || 0;
-        console.log(`> sizes repos: ${ sizes }`)
-
-        // LANGUAGES
-        languages = new Array();
-        for (var i=0; i<RESPONSE.length; i++) {
-            languages.push(RESPONSE[i].language)
-        }
-        languages_counted = {}
-        for (const element of languages) {
-            if(languages_counted[element]) {
-                languages_counted[element]+=1
+    // new request: https://api.github.com/user/<id>/repos?page=<idpage>
+    for(var page=1; page<10; page++) {
+        axios.get(`https://api.github.com/user/${ ID }/repos?page=${ page }`).then(response => {
+            if(response.data==[]){
+                console.log("PAGE IS EMPTY")
             } else {
-                languages_counted[element]=1
+                console.log("PAGE IS NOT EMPTY")
+                MAX_PAGES+=1
             }
-        }
-        languages_keys = Object.keys(languages_counted)
-        languages_values = Object.values(languages_counted)
-        labels = languages_keys
-        indexes = new Array()
-        for (var i=0; i<labels.length; i++) {
-            indexes.push(
-                colorsIndexes[Math.floor(Math.random()*colorsIndexes.length)]
-            )
-        }
-        backgroundColor = new Array();
-        borderColor = new Array();
-        for (var colorToAppend=0; colorToAppend<indexes.length; colorToAppend++) {
-            backgroundColor.push(colorsGraph[colorToAppend].bg)
-            borderColor.push(colorsGraph[colorToAppend].border)
-        }
-        borderWidth = 1;
-        datag = languages_values;
-
-        // CREATION HOUR
-        for (var i=0; i<RESPONSE.length; i++) {
-            created_at.push(RESPONSE[i].created_at.slice(11, 13))
-        }
-        created_at.forEach(hour => {
-            let int_hour = parseInt(hour)
-            // console.log(`value: ${int_hour} - type: ${typeof(int_hour)}`)
-            if(int_hour>0 && int_hour<5){
-                hours_dict['0->5']+=1
-            } else if(int_hour>=5 && int_hour<10){
-                hours_dict['5->10']+=1
-            } else if(int_hour>=15 && int_hour<20){
-                hours_dict['15->20']+=1
-            } else if(int_hour>=20 && int_hour<25){
-                hours_dict['20->0']+=1
-            }
+        }).catch(error => {
+            console.error("> error")
         })
-        hours = Object.keys(hours_dict)
-        created_at_list = Object.values(hours_dict)
+    }
+    console.log(`MAX_PAGES: ${ MAX_PAGES }`)
+    // axios.get(`https://api.github.com/users/${ DEVELOPER }/repos`).then(response => {
+    //     console.log(`> response status for user repos: ${ response.status }`)
+    //     RESPONSE = response.data
+    //     console.log(`> response length: ${ RESPONSE.length }`)
 
-        // REPO INIT FREQUENCY
-        creation_date = new Array()
-        for (var i=0; i<RESPONSE.length; i++) {
-            creation_date.push(new Date(RESPONSE[i].created_at))
-        }
-        creation_date.sort((date1, date2) => date1-date2)
-        console.log(`> creation dates: ${ creation_date }`)
-        gaps = new Array();
-        for (var i=0; i<creation_date.length; i++) {
-            if(i==creation_date.length-1){
-                break
-            } else {
-                let gap = parseInt(Number((creation_date[i+1]-creation_date[i])/(1000*3600*24)).toFixed(0))
-                gaps.push(gap)
-            }
-        }
-        console.log(`> gaps: ${ gaps }`)
-        sum_freq=gaps.reduce((a, b) => a+b, 0)
-        freq_repo = (sum_freq/gaps.length) || 0;
-        freq_repo = parseInt(Number((freq_repo).toFixed(0)))
 
-        param_res.render("users/dashboard", { 
-            developer: param_req.params.devId, 
-            data: RESPONSE, 
-            avg_size: avg_size,
-            avg_commits: avg_commits,
-            freq_repo: freq_repo,
+
+    //     // REPOS SIZES
+    //     sizes = new Array()
+    //     for (var i = 0; i < RESPONSE.length; i++) {
+    //         sizes.push(RESPONSE[i].size)
+    //     }
+    //     sum = sizes.reduce((a, b) => a+b, 0)
+    //     avg_size = Number(((sum / sizes.length)/1000).toFixed(1)) || 0;
+    //     console.log(`> sizes repos: ${ sizes }`)
+
+    //     // LANGUAGES
+    //     languages = new Array();
+    //     for (var i=0; i<RESPONSE.length; i++) {
+    //         languages.push(RESPONSE[i].language)
+    //     }
+    //     languages_counted = {}
+    //     for (const element of languages) {
+    //         if(languages_counted[element]) {
+    //             languages_counted[element]+=1
+    //         } else {
+    //             languages_counted[element]=1
+    //         }
+    //     }
+    //     languages_keys = Object.keys(languages_counted)
+    //     languages_values = Object.values(languages_counted)
+    //     labels = languages_keys
+    //     indexes = new Array()
+    //     for (var i=0; i<labels.length; i++) {
+    //         indexes.push(
+    //             colorsIndexes[Math.floor(Math.random()*colorsIndexes.length)]
+    //         )
+    //     }
+    //     backgroundColor = new Array();
+    //     borderColor = new Array();
+    //     for (var colorToAppend=0; colorToAppend<indexes.length; colorToAppend++) {
+    //         backgroundColor.push(colorsGraph[colorToAppend].bg)
+    //         borderColor.push(colorsGraph[colorToAppend].border)
+    //     }
+    //     borderWidth = 1;
+    //     datag = languages_values;
+
+    //     // CREATION HOUR
+    //     for (var i=0; i<RESPONSE.length; i++) {
+    //         created_at.push(RESPONSE[i].created_at.slice(11, 13))
+    //     }
+    //     created_at.forEach(hour => {
+    //         let int_hour = parseInt(hour)
+    //         // console.log(`value: ${int_hour} - type: ${typeof(int_hour)}`)
+    //         if(int_hour>0 && int_hour<5){
+    //             hours_dict['0->5']+=1
+    //         } else if(int_hour>=5 && int_hour<10){
+    //             hours_dict['5->10']+=1
+    //         } else if(int_hour>=15 && int_hour<20){
+    //             hours_dict['15->20']+=1
+    //         } else if(int_hour>=20 && int_hour<25){
+    //             hours_dict['20->0']+=1
+    //         }
+    //     })
+    //     hours = Object.keys(hours_dict)
+    //     created_at_list = Object.values(hours_dict)
+
+    //     // REPO INIT FREQUENCY
+    //     creation_date = new Array()
+    //     for (var i=0; i<RESPONSE.length; i++) {
+    //         creation_date.push(new Date(RESPONSE[i].created_at))
+    //     }
+    //     creation_date.sort((date1, date2) => date1-date2)
+    //     // console.log(`> creation dates: ${ creation_date }`)
+    //     gaps = new Array();
+    //     for (var i=0; i<creation_date.length; i++) {
+    //         if(i==creation_date.length-1){
+    //             break
+    //         } else {
+    //             let gap = parseInt(Number((creation_date[i+1]-creation_date[i])/(1000*3600*24)).toFixed(0))
+    //             gaps.push(gap)
+    //         }
+    //     }
+    //     console.log(`> gaps: ${ gaps }`)
+    //     sum_freq=gaps.reduce((a, b) => a+b, 0)
+    //     freq_repo = (sum_freq/gaps.length) || 0;
+    //     freq_repo = parseInt(Number((freq_repo).toFixed(0)))
+
+    //     param_res.render("users/dashboard", { 
+    //         developer: param_req.params.devId, 
+    //         data: RESPONSE, 
+    //         avg_size: avg_size,
+    //         avg_commits: avg_commits,
+    //         freq_repo: freq_repo,
     
-            labels: labels,
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            borderWidth: borderWidth,
-            datag: datag,
+    //         labels: labels,
+    //         backgroundColor: backgroundColor,
+    //         borderColor: borderColor,
+    //         borderWidth: borderWidth,
+    //         datag: datag,
     
-            hours: hours,
-            created_at_list: created_at_list
-        });
-    })
+    //         hours: hours,
+    //         created_at_list: created_at_list
+    //     });
+    // })
 });
 
 app.listen(3000)
